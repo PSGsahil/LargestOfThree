b30f13bd87651e17662220e2b7897681d3e49761
21qfmp1 <ankitghodke24@gmail.com>
2023-07-25 08:22:54 +0530

    Src file updated

--- Main.java
+++ Main.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter the third number: ");
+        int num3 = sc.nextInt();
     }
 }
