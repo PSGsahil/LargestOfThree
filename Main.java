b38332cc3d0e0a41d70d3d526379ed4cc0547235
21qfmp1 <gudekarpranay@gmail.com>
2023-07-25 08:47:41 +0530

    Logic ic added

--- Main.java
+++ Main.java
@@ -9,5 +9,21 @@ public class Main {
         int num2 = sc.nextInt();
         System.out.print("Enter the third number: ");
         int num3 = sc.nextInt();
+        int largestNumber=0;
+        if (num2 > largestNumber) {
+            largestNumber = num2;
+        }
+
+        if (num3 > largestNumber) {
+            largestNumber = num3;
+        }
+
+        double sumOfLargest = largestNumber;
+        System.out.println("The sum of the largest number is: " + sumOfLargest);
+
+        sc.close();
+    }
+}
+
     }
 }
